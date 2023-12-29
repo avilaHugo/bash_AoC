d42b18064bc97878b4c5a290d98373fb21d5bc35
Hugo L. Ávila <53014804+avilaHugo@users.noreply.github.com>
2023-12-29 19:52:38 -0300

    Better day 4 with awk.

--- 2023/day_4/solution.sh
+++ 2023/day_4/solution.sh
@@ -1,40 +1,48 @@
 #!/usr/bin/env bash
 
-get_winning_numbers() {
-    local winning=${1}
-    local card=${2}
+# USAGE: this.sh < input.txt
 
-    grep -xFf \
-        <(tr ' ' '\n' <<< "${winning}") \
-        <(tr ' ' '\n' <<< "${card}")   \
-        | grep '[[:digit:]]'
+sed -r '
+        s/\:\s+/ | /;
+        s/\s\|\s/,/g;
+        s/[[:space:]]+/ /g;
+    ' \
+    | awk -F',' '
+        {
+            split($2, arr, " ");
+            for (i in arr) { win_num[arr[i]]=0 }
 
-}
+            split($3, arr, " ");
 
-# part_1 
-while IFS='|' read -r WINNING CARD ;do
-    get_winning_numbers "${WINNING}" "${CARD}" \
-        | wc -l 
-done | awk '$0 >= 1 { sum+=2^($0-1) } END { print sum }'
+            c=0
+            for (i in arr) {
+                if (arr[i] in win_num) {c++}
+            }
 
+            print c
+            delete win_num
+        }
+    ' \
+    | awk '
+        {
+            cards_count[NR]=1
+            scores[NR]=$0
+        }
 
-# part_2
-declare -A game
-while IFS='|' read -r WINNING CARD ;do
-    read -r game_id < <( [[ "${WINNING}" =~ ([[:digit:]]+) ]]; echo "${BASH_REMATCH[0]}" )
-    read -r winning_numbers_count < <( get_winning_numbers "${WINNING}" "${CARD}" | wc -l)
+        END {
 
-    # 0: wins
-    game["${game_id}",0]="${winning_numbers_count}"
+            for (i in cards_count) {
+                for (j = i+1; j <= i+scores[i]; j++) {
+                    cards_count[j]+=cards_count[i]
+                }
+            }
 
-    # 1: total card
-    game["${game_id}",1]=1
-done
+            for (i in cards_count) {
+                p1+= scores[i] > 0 ? 2^(scores[i]-1) : 0
+                p2+=cards_count[i]
+            }
 
-for ((i=1; i<=$(( "${#game[@]}" / 2 )); i++));do
-    wins=game["${i}",0]
-    for (( j=$(( ${i} + 1 )); j <=  $(( ${i} + ${wins} )); j++));do
-       game["${j}",1]=$(( ${game["${j}",1]} + ${game["${i}",1]} ))
-    done
-    echo "${game[${i},1]}"
-done | awk '{sum+=$0} END {print sum}'
+            print "Part 1:" p1
+            print "Part 2:" p2
+        }
+    '

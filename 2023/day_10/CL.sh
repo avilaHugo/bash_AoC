9417e589d24ac7e670c674a1d5cf0938a32fc432
Hugo L. Ávila <53014804+avilaHugo@users.noreply.github.com>
2023-12-31 19:36:24 -0300

    Day 10: part 1 working.

--- 2023/day_10/CL.sh
+++ 2023/day_10/CL.sh
@@ -1,61 +1,68 @@
 #!/usr/bin/env bash
 
 while read -r LINE;do
-    grep -Pob --label=$((c++)) -H  '[\|\-LJ7FS]' <<< "${LINE}"
+    grep -Pob --label=$((c++)) -H  '[\.\|\-LJ7FS]' <<< "${LINE}"
 done \
     | awk -F ':' '
        function iter_adj(i, j, pos_mat) {
             delete pos_mat
-            l=0
+            curr_square_pos=0
+            curr_arr_index=0
             for (x=i-1; x<=i+1; x++) {
                 for (z=j-1; z<=j+1; z++) {
-                    if (x == i && z == j) { continue }
-                    l++
-                    pos_mat[l][1]=x
-                    pos_mat[l][2]=z
+                    curr_square_pos++
+                    if ( ( curr_square_pos % 2 != 0 ) || ( x == i && z == j ) ) { continue }
+                    curr_arr_index++
+                    pos_mat[curr_arr_index]["i"]=x
+                    pos_mat[curr_arr_index]["j"]=z
                 }
             }
-            for (x=1; x<=9; x=x+2) {
-                delete pos_mat[x]
-            }
        }
-
-       # function is_path(i, j, I, J, symbol) {
-            # 
-       # }
 
        $3 == "S" { s_i=$1; s_j=$2 }
 
        { game[$1][$2]=$3 }
 
        END {
-            split("| - L J 7 F", symbols, " ")
-            split("-1,1 0,0 -1,0 -1,0 0,1 0,1", is, " ")
-            split("0,0 -1,1 0,1 -1,0 -1,0 0,1", js, " ")
-            for (i in symbols) {
-                split(is[i], arr, ",")
-                sym_map[symbols[i]]["i"][arr[1]]=1
-                sym_map[symbols[i]]["i"][arr[2]]=1
 
-                split(js[i], arr, ",")
-                sym_map[symbols[i]]["j"][arr[1]]=1
-                sym_map[symbols[i]]["j"][arr[2]]=1
+            curr_i=s_i
+            curr_j=s_j
+            split("[|7F] [-LF] [-J7] [|LJ]", allowed_symbol, " ")
+            split("N W E S", pos, " ")
+
+            symbols["|"]="[NS]"
+            symbols["-"]="[WE]"
+            symbols["L"]="[NE]"
+            symbols["J"]="[NW]"
+            symbols["7"]="[SW]"
+            symbols["F"]="[SE]"
+            symbols["S"]="[NEWS]"
+
+            while (1) {
+                curr_symbol=game[curr_i][curr_j]
+
+                iter_adj(curr_i, curr_j, pos_mat)
+                for (x in pos_mat) {
+                    i=pos_mat[x]["i"]
+                    j=pos_mat[x]["j"]
+                    adj_sym = game[i][j]
+
+                    if ( pos[x] !~ symbols[curr_symbol] ) { continue  }
+                    if ( adj_sym !~ allowed_symbol[x] ) { continue }
+                    if ( visited[i][j]++ ) { continue  }
+                    curr_i=i
+                    curr_j=j
+                    break
+                }
+
+                ++c
+                if (c == 2) { for (x in allowed_symbol) { sub("]", "S]", allowed_symbol[x]) } }
+                if ( c > 2 && curr_symbol == "S" ) {break}
+
             }
-
-            curr_i="Na"
-            curr_j="Na"
-
-            iter_adj(s_i, s_j, pos_mat)
-
-            for (x in pos_mat) {
-                i = pos_mat[x][1]
-                j = pos_mat[x][2]
-                symbol = game[i][j]
-            }
-
-            # while ( s_i != curr_i && s_j != curr_j ) {
-                # break
-            # }
+            print (c - 1) / 2    
 
        }
     '
+
+

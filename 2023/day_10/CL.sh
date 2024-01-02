4ff368e24df7c8729d7903b36ec7cff4205b0fef
Hugo L. Ávila <53014804+avilaHugo@users.noreply.github.com>
2024-01-02 10:48:43 -0300

    intervals woking.

--- 2023/day_10/CL.sh
+++ 2023/day_10/CL.sh
@@ -27,6 +27,7 @@ done \
 
             curr_i=s_i
             curr_j=s_j
+            tile_loop[curr_i][curr_j]=1
             split("[|7F] [-LF] [-J7] [|LJ]", allowed_symbol, " ")
             split("N W E S", pos, " ")
 
@@ -52,6 +53,7 @@ done \
                     if ( visited[i][j]++ ) { continue  }
                     curr_i=i
                     curr_j=j
+                    tile_loop[i][j]=1
                     break
                 }
 
@@ -60,7 +62,57 @@ done \
                 if ( c > 2 && curr_symbol == "S" ) {break}
 
             }
-            print (c - 1) / 2    
+            print "Part 1:", (c - 1) / 2
+
+            I = $1
+            J = $2
+
+            for (i=0; i<=I; i++) {
+                j1 = 0
+                j2 = 0
+                t = 0
+                while (j1 < J) {
+                    
+                    j2 = j1
+
+                    while ( j2 < J && tile_loop[i][j2+1] && tile_loop[i][j2] ) {
+                        j2++
+                    }
+
+                    if ( j1 == j2 ) {
+
+                        if ( tile_loop[i][j1] ) {
+                            curr_inter[t][j1]++
+                        }
+
+                        j1++
+                        continue
+                    }
+
+                    curr_inter[t][j1]++
+                    curr_inter[t][j2]++
+
+                    j1 = j2 + 1
+                    t++
+                    
+                }
+
+
+                print ""
+                print "I :"i
+                for (G in curr_inter){
+                    for (H in curr_inter[G]){
+                        printf("%s ", H)
+                    }
+                    print ""
+                }
+                delete curr_inter
+                
+
+
+            }
+
+            print "Part 2: "total
 
        }
     '
